a7c454d80b0cd8b46e3d6399ff9eeb32642601f4
rafaelcalpena <rafael.calpena@gmail.com>
2021-03-26 03:24:03 -0300

    feat: add more heuristics

--- src/heuristic.ts
+++ src/heuristic.ts
@@ -1,7 +1,7 @@
 import {Domain} from './classes';
 
 /* TODO: Add more heuristics */
-export const heuristic = <T>(domain: Domain<T>): [string, T] => {
+export const getFirstHeuristic = <T>(domain: Domain<T>): [string, T] => {
     let result!: [string, T];
     /* Return first literal found */
     for (let [k, v] of domain) {
@@ -9,3 +9,39 @@ export const heuristic = <T>(domain: Domain<T>): [string, T] => {
     }
     return result;
 }
+
+export const sortedAlphabetically = <T>(domain: Domain<T>): [string, T] => {
+    let variable = [...domain.firstKeys()].sort()[0];
+    return [variable, [...domain.get(variable)].sort()[0]];
+}
+
+/* Smallest Maybe First (SMF) selects a variable with the smallest non-zero number 
+of literals in SD′ \ ValsIn′ */
+/* TODO: Abstract to Bidirectional Tuple Set */
+export const SMF = <T>(domain: Domain<T>): [string, T] => {
+    let sorted = [...domain.firstKeys()]
+        .map(key => ({key, size: domain.get(key).size}))
+        .filter(d => d.size > 0)
+        .sort((a,b) => a.size-b.size);
+    let smallestLiteral = [...domain.get(sorted[0].key)].sort()[0];
+    return [sorted[0].key, smallestLiteral];
+}
+
+export const random = <T>(domain: Domain<T>): [string, T] => {
+    let domainArr = [...domain];
+    return domainArr[Math.floor(Math.random() * domainArr.length)]
+}
+
+export const heuristic = (() => {
+    switch (process.env.HEU) {
+        case 'first':
+            return getFirstHeuristic;
+        case 'SMF':
+            return SMF;
+        case 'random':
+            return random;
+        case 'alphabetical':
+        default:
+            return sortedAlphabetically;
+    }
+})();
