07d2337203bb56e4d0ab557817d1dcc3a53d4e6c
rafaelcalpena <rafael.calpena@gmail.com>
2021-03-26 03:25:41 -0300

    feat: code for genTree algorithm

--- src/operations.ts
+++ src/operations.ts
@@ -1,9 +1,13 @@
 export const DIFFERENT = '!=';
-export type Operation = typeof DIFFERENT;
+export const OR = '||';
+export type Operation = typeof DIFFERENT | typeof OR;
 
 /* Checks for ensuring some constraint is valid or not */
 export const check = {
     [DIFFERENT]: <T>(a: T, b: T) => {
         return a !== b;
+    },
+    [OR]: <T>(a: T, b: T) => {
+        return a || b;
     }
 }
--- src/utils/create-graph-json.ts
+++ src/utils/create-graph-json.ts
@@ -2,10 +2,17 @@ import { PropagatorTreeNode } from "../classes";
 
 let id = 0;
 /* TODO: Improve types */
-export const createGraphJSON = <T>(node: PropagatorTreeNode<T>, step: any[] = [], parent?: number): any[] => {
+export const createGraphJSON = <T>(node: PropagatorTreeNode<T> | null, step: any[] = []): any[] => {
+
+    if (node === null) {
+        return step;
+    }
+
     const item: any = {
         id: `${id}`,
         label: `<Test:(${node.test}), Prune:${node.prune}>`,
+        test: node.test,
+        prune: [...node.prune]
     };
     step.push({
         data: item
@@ -18,21 +25,23 @@ export const createGraphJSON = <T>(node: PropagatorTreeNode<T>, step: any[] = []
                 source: `${parentId}`,
                 id: `${parentId}-${id}`,
                 target: `${id}`,
+                isDeleteBranch: false,
                 label: `${(node.test as [string, T])[1]} &#8712; D(${(node.test as [string, T])[0]})`
             }
         })
-        createGraphJSON(node.left, step, id);
+        createGraphJSON(node.left, step);
     }
     if (node.right) {
         step.push({
             data: {
                 source: `${parentId}`,
                 id: `${parentId}-${id}`,
+                isDeleteBranch: true,
                 target: `${id}`,
                 label: `${(node.test as [string, T])[1]} &#8713; D(${(node.test as [string, T])[0]})`
             }
         })       
-        createGraphJSON(node.right, step, id);
+        createGraphJSON(node.right, step);
     }
     return step;
 }
