bbdbca76c7ea3976003f7f19c4adbad8f54556e8
Rafael Calpena Rodrigues <rafael.calpena@gmail.com>
2022-05-07 23:41:37 -0300

    chore: job array of combinations

--- cloud/start.sh
+++ cloud/start.sh
@@ -5,6 +5,6 @@ pulumi up -y -s dev
 
 # Trigger job start
 # TODO: Use dynamic size here
-aws batch submit-job --job-name partial-test --job-queue $(pulumi stack output jobQueueArn -s dev) --job-definition $(pulumi stack output jobDefinitionArn -s dev) --region us-east-1 --array-properties size=9
+aws batch submit-job --job-name partial-test --job-queue $(pulumi stack output jobQueueArn -s dev) --job-definition $(pulumi stack output jobDefinitionArn -s dev) --region us-east-1 --array-properties size=$(node -e 'let m = require("../graph-coloring/testbench/config.js"); console.log(m.graphs.length * m.algorithms.length)')
 
 echo "https://us-east-1.console.aws.amazon.com/batch/home?region=us-east-1#jobs"
